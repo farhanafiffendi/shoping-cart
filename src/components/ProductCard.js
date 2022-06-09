31f4dc81ae306d84ee9448675e7ee4d7be6a6deb
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-06-09 13:56:06 +0700

    first commit

---
+++ src/components/ProductCard.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import "./ProductCard.css";
+import convertRupiah from 'rupiah-format'
+import Rating from "./Rating";
+import CartContext from "../context/cart/CartContext";
+
+const ProductCard = ({ product }) => {
+  const { addToCart } = useContext(CartContext);
+
+  return (
+    <div className='productCard__wrapper'>
+      <div>
+        <img className='productCard__img' src={product.image} alt='' />
+        <h4>{product.name}</h4>
+        <div className='ProductCard__price'>
+          <h5>{convertRupiah.convert(product.price)}</h5>
+        </div>
+        <div className='ProductCard__Rateing'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </div>
+        <button
+          className='ProductCard__button'
+          onClick={() => addToCart(product)}
+        >
+          Add to basket
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
